f19d30ff27487a7f97f791543a4212cdec80d86d
Vu Tung Lam <vutunglaminfo@gmail.com>
2025-12-18 06:16:25 +0700

    maybe we need it...

--- customer-web/src/services/socketService.ts
+++ customer-web/src/services/socketService.ts
@@ -38,7 +38,7 @@ class SocketService {
     this.socket.on('connect', () => {
       console.log('Socket.IO connected:', this.socket?.id);
       // Re-register all callbacks on reconnection
-      // this.reregisterCallbacks();
+      this.reregisterCallbacks();
       // Re-join room if there was one
       if (this.currentRoom) {
         this.socket?.emit('join', { conversation_id: this.currentRoom });
@@ -48,6 +48,7 @@ class SocketService {
 
     this.socket.on('disconnect', (reason) => {
       console.log('Socket.IO disconnected:', reason);
+      this.socket?.removeAllListeners();
     });
 
     this.socket.on('connect_error', (error) => {
